201c5666a5107f566c9234acb499e3dfdc47bc44
SikyChen <chensiqi247@gmail.com>
2022-04-03 12:04:05 +0800

    add: stack

--- stack/stack-object.test.js
+++ stack/stack-object.test.js
@@ -1,21 +1,21 @@
 const Stack = require('./stack-object');
 
 const stack = new Stack();
-stack.print();
+stack.print();      // Stack: empty
 
 stack.push(1);
-stack.print();
+stack.print();      // Stack: 1
 
 stack.push(2,3,4,5);
-stack.print();
-console.log('size:', stack.size())
-console.log('toString', stack.toString());
+stack.print();      // Stack: 1,2,3,4,5
+console.log('size:', stack.size())          // size: 5
+console.log('toString', stack.toString());  // toString 1,2,3,4,5
 
-console.log('pop:', stack.pop());
-stack.print();
-console.log('size:', stack.size())
+console.log('pop:', stack.pop());           // pop: 5
+stack.print();                              // Stack: 1,2,3,4
+console.log('size:', stack.size())          // size: 4
 
-console.log('isEmpty:', stack.isEmpty());
-console.log('clear:', stack.clear());
-stack.print();
-console.log('isEmpty:', stack.isEmpty());
+console.log('isEmpty:', stack.isEmpty());   // isEmpty: false
+console.log('clear:', stack.clear());       // clear: true
+stack.print();                              // Stack: empty
+console.log('isEmpty:', stack.isEmpty());   // isEmpty: true
