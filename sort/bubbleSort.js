2821b72b06938591b172f7945bf6d9485bcb04d3
SikyChen <chensiqi247@gmail.com>
2022-06-10 09:53:41 +0800

    add: bubbleSort

--- sort/bubbleSort.js
+++ sort/bubbleSort.js
@@ -12,7 +12,7 @@
  */
 module.exports = bubbleSort = arr => {
   for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; i < arr.length - 1 - i; j++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
       }
--- sort/sort.test.js
+++ sort/sort.test.js
@@ -1,11 +1,11 @@
+const { divider } = require("../utils");
 const bubbleSort = require("./bubbleSort");
 
 const originArray = [];
-for(let i=0; i<100; i++) {
-  originArray.push(Math.floor(Math.random() * 1000));
+for(let i=0; i<10000; i++) {
+  originArray.push(Math.floor(Math.random() * 10000));
 }
 const sortedArray = originArray.concat().sort((a, b) => a - b);
-console.log('sortedArray')
 
 function inspector(sorter) {
   if (typeof sorter !== 'function') {
@@ -13,9 +13,9 @@ function inspector(sorter) {
   }
 
   const originArrayTemp = originArray.concat();
-  console.time('speed');
-  const resultArray = sorter(originArrayTemp) || [];console.log(4444)
-  console.timeEnd('speed');
+  console.time('🏃 speed');
+  const resultArray = sorter(originArrayTemp) || [];
+  console.timeEnd('🏃 speed');
 
   if (resultArray.length !== originArrayTemp.length) {
     return console.error('排序结果有误');
@@ -30,4 +30,6 @@ function inspector(sorter) {
   console.log('✅ 排序结果正确')
 }
 
+divider();
+console.log('bubbleSort: ');
 inspector(bubbleSort);
